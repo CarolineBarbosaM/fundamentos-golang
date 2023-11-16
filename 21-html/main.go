edaba9b906cc6645a9b6e17046ba3a0accddcfc3
Caroline Martins <53910116+CarolineBarbosaM@users.noreply.github.com>
2023-11-16 16:42:45 -0300

    feat: html

---
+++ 21-html/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var templates *template.Template
+
+type usuario struct {
+	Nome  string
+	Email string
+}
+
+func main() {
+	templates = template.Must(template.ParseGlob("*.html"))
+
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+		u := usuario{"José", "jose.da.silva@email.com"}
+		templates.ExecuteTemplate(w, "home.html", u)
+	})
+
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
